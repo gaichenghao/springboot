6cd7378291948191c08d470e2fbb1fad4681d758
gaich <1432351805@qq.com>
2021-10-22 17:30:18 +0800

    dao34

--- boot-05-web-01/src/main/java/com/atguigu/boot/config/WebConfig.java
+++ boot-05-web-01/src/main/java/com/atguigu/boot/config/WebConfig.java
@@ -3,9 +3,12 @@ package com.atguigu.boot.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UrlPathHelper;
 
 @Configuration(proxyBeanMethods = false)
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
@@ -14,4 +17,25 @@ public class WebConfig {
         return hiddenHttpMethodFilter;
     }
 
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void configurePathMatch(PathMatchConfigurer configurer) {
+                UrlPathHelper urlPathHelper=new UrlPathHelper();
+                //不移除：后面的内容。矩阵变量功能就可以实现
+                urlPathHelper.setRemoveSemicolonContent(false);
+                configurer.setUrlPathHelper(urlPathHelper);
+            }
+        };
+    }
+
+     //WebMvcConfigurer
+//    @Override
+//    public void configurePathMatch(PathMatchConfigurer configurer) {
+//        UrlPathHelper urlPathHelper=new UrlPathHelper();
+//        //不移除：后面的内容。矩阵变量功能就可以实现
+//        urlPathHelper.setRemoveSemicolonContent(false);
+//        configurer.setUrlPathHelper(urlPathHelper);
+//    }
 }
---
+++ boot-05-web-01/src/main/java/com/atguigu/boot/controller/ParameterTestController.java
@@ -0,0 +1,80 @@
+package com.atguigu.boot.controller;
+
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class ParameterTestController {
+
+
+    @GetMapping("/car/{id}/owner/{username}")
+    public Map<String,Object> getCar(@PathVariable("id") Integer id,
+                                     @PathVariable("username") String name,
+                                     @PathVariable Map<String,String> pv,
+                                     @RequestHeader("User-Agent") String userAgent,
+                                     @RequestHeader Map<String,String> heads,
+                                     @RequestParam("age") Integer age,
+                                     @RequestParam("inters")List<String> inters,
+                                     @RequestParam Map<String,String> maps,
+                                     @CookieValue("TopicAddress") String topicAddress,
+                                     @CookieValue("TopicAddress") Cookie cookie){
+        Map<String,Object> map=new HashMap<>();
+//        map.put("id",id);
+//        map.put("username",name);
+//        map.put("pv",pv);
+//        map.put("userAgent",userAgent);
+//        map.put("heads",heads);
+        map.put("age",age);
+        map.put("inters",inters);
+        map.put("maps",maps);
+        map.put("topicAddress",topicAddress);
+        map.put("cookie",cookie);
+        return map;
+    }
+
+    @PostMapping("/save")
+    public Map postMethod(@RequestBody String content){
+        Map<String,Object> map=new HashMap<>();
+        map.put("content",content);
+        return map;
+
+    }
+
+
+    //1.语法"/car/sell;low=34;brand=byd,audi,yd"
+    //2.SpringBoot默认是禁用了矩阵变量的功能
+    //3.手动开启：原理。对于路径的处理。UrlPathHelper 进行解析。
+    //removeSemicolonContent(移除分号内容) 支持矩阵变量的
+    //3、矩阵变量必须有url路径才能被解析
+    @GetMapping("/car/{path}")
+    public Map carsSell(@MatrixVariable Integer low,
+                        @MatrixVariable List<String> brand,
+                        @PathVariable("path") String path){
+        Map<String,Object> map=new HashMap<>();
+        map.put("low",low);
+        map.put("brand",brand);
+        map.put("path",path);
+        return map;
+    }
+
+    //"/boss/1;age=20/2;age=10"
+    @GetMapping("/boss/{bossId}/{empId}")
+    public Map boss(@MatrixVariable(value = "age",pathVar = "bossId") Integer bossAge,
+                    @MatrixVariable(value = "age",pathVar = "empId") Integer empAge){
+        Map<String,Object> map=new HashMap<>();
+        map.put("bossAge",bossAge);
+        map.put("empAge",empAge);
+        return map;
+    }
+
+
+
+}
